716ea1de58ed5c38db0beb09ccdccc814f947b6b
Cody Frank <cody.frank30@gmail.com>
2019-05-25 02:37:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,19 +45,19 @@ class Owner
       dog.mood = "happy"
     end
   end
-  
+
   def play_with_cats
     @pets[:cats].each do|cat|
       cat.mood = "happy"
     end
   end
-  
+
   def feed_fish
     @pets[:fishes].each do|fish|
       fish.mood = "happy"
     end
   end
-  
+
   def sell_pets
     @pets.each do |species, array|
       array.each do |animal|
@@ -73,5 +73,5 @@ class Owner
     num_dogs = @pets[:dogs].length
     "I have #{num_fish}fish, #{num_dogs}dog(s), and #{num_cats}cat(s)."
   end
-  
+
 end
