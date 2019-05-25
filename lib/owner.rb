87f1cdb9e17aa614506bb75c48c1d738a346155b
Cody Frank <cody.frank30@gmail.com>
2019-05-25 02:10:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,20 +1,20 @@
 class Owner
   @@all = []
-  
+
   def initialize
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.count
     @@all.length
   end
-  
+
   def self.reset_all
     @@all.clear
   end
-  
+
 end
